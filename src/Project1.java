c8ef88c773dabf065d7b71864ad7ddee9b3a70a1
Christopher Naro <cnaro1996@gmail.com>
2019-06-30 01:17:37 -0400

    Add heuristic calculator method.

--- src/Project1.java
+++ src/Project1.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.math.*;
 
 public class Project1 {
 
@@ -28,6 +29,10 @@ public class Project1 {
         return gridworld;
     }
 
+    /**
+     * Prints a character graphic representation of the specified gridworld.
+     * @param gridworld
+     */
     public static void printGrid(boolean[][] gridworld){
         for (int i=0; i<gridworld.length; i++){
             for (int j=0; j<gridworld.length; j++){
@@ -41,5 +46,43 @@ public class Project1 {
         }
     }
 
+    /**
+     * An A* algorithm implemented utilizing a min binary heap structure.
+     * This method assumes all gridworld spaces are available to see by
+     * the agent.
+     * @param gridworld The gridworld to apply the algorithm to.
+     * @return True if solvable, false if not.
+     */
+    public static boolean aStarSearch(boolean[][] gridworld){
+        
+    }
 
+    /**
+     * The heuristic types specified by the project.
+     */
+    public enum Type {
+        EUCLIDIAN,
+        MANHATTAN,
+        CHEBYSHEV;
+    }
+
+    /**
+     * Calculates the heuristic between two given states.
+     * @param type The type of heuristic formula to use for calculations.
+     * @param x1
+     * @param x2
+     * @param y1
+     * @param y2
+     * @return The specified heuristic between two states
+     */
+    public static double heuristicCalc(Type type, int x1, int y1, int x2, int y2){
+        if(type == Type.EUCLIDIAN) {
+            return Math.sqrt((x1-x2)^2 + (y1-y2)^2);
+        } else if(type == Type.MANHATTAN) {
+            return Math.abs(x1-x2) + Math.abs(y1-y2);
+        } else {
+            //CHEBYSHEV
+            return Math.max(Math.abs(x1-x2), Math.abs(y1-y2));
+        }
+    }
 }
